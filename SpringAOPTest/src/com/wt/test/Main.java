52f3d376a4018bd50b8385fabe75bf9d4b0d4d93
hohoTT <609029365@qq.com>
2016-06-14 11:11:36 +0800

    添加使用动态代理的使用情况，简化代码的操作，提高代码的可读性

--- SpringAOPTest/src/com/wt/test/Main.java
+++ SpringAOPTest/src/com/wt/test/Main.java
@@ -2,18 +2,26 @@ package com.wt.test;
 
 import com.wt.interfaces.ArithmetiCalculator;
 import com.wt.interfaces.impl.ArithmetiCalculatorImpl;
+import com.wt.interfaces.impl.ArithmetiCalculatorLoggingImpl;
+import com.wt.proxy.ArithmetiCalculatorLoggingProxy;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		ArithmetiCalculator arithmetiCalculator = null;
-		arithmetiCalculator = new ArithmetiCalculatorImpl();
+		// ArithmetiCalculator arithmetiCalculator = null;
+		// arithmetiCalculator = new ArithmetiCalculatorLoggingImpl();
 
-		int result = arithmetiCalculator.add(1, 2);
+		// 使用动态代理
+		ArithmetiCalculator target = new ArithmetiCalculatorImpl();
+		ArithmetiCalculator proxy = new ArithmetiCalculatorLoggingProxy(target)
+				.getLoggingProxy();
+		
+
+		int result = proxy.add(1, 2);
 		System.out.println("result --- " + result);
 
-		result = arithmetiCalculator.div(4, 2);
+		result = proxy.div(4, 2);
 		System.out.println("result --- " + result);
 
 	}
